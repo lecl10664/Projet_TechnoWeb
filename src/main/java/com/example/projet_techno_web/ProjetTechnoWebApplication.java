e70ecbe2c2a6aa255ac7e89057928ede1b1ec96a
lecl10664 <58337915+lecl10664@users.noreply.github.com>
2020-12-15 12:36:58 +0100

    Initial commit

---
+++ src/main/java/com/example/projet_techno_web/ProjetTechnoWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_techno_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetTechnoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetTechnoWebApplication.class, args);
+    }
+
+}
