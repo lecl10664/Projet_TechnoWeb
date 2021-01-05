e80b3a0f5a721d369b7b3b205c378f8668d12963
lecl10664 <58337915+lecl10664@users.noreply.github.com>
2021-01-05 19:01:25 +0100

    Update logementListe.js

--- src/main/java/view/js/logementListe.js
+++ src/main/java/view/js/logementListe.js
@@ -5,13 +5,19 @@ function showLogement() {
         .then(response => response.json())
         .then(data => {
             let tableInfo = "";
-            data.forEach(element => tableInfo+=
-                "<tr>" +
-                "<td>" + element.id +"</td>" +
-                "<td>" + element.nom + "</td>" +
-                "<td>" + element.type + "</td>" +
-                "<td><button>Voir le logement</button></td>" +
-                "</tr>")
+            data.forEach(element => tableInfo+=`
+                <tr>
+                <td>${element.id}</td>
+                <td>${element.nom}</td>
+                <td>${element.type}</td>
+                <td><button onclick='navigate(${element.id})'>Voir le logement</button></td>
+                </tr>
+            `  )
             document.querySelector("#listTable").innerHTML = tableInfo;
         })
+}
+
+
+function navigate(logementId) {
+    window.location.href='/logement.html?id=' + logementId.value();
 }
